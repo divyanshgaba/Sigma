ff65971aea6f05b37a1544dba3ad03f43ac0d747
Karandeep Singh <karan.redrock@gmail.com>
2018-11-17 20:55:07 +0530

    box color added and overlay fix

--- js/custom.js
+++ js/custom.js
@@ -9,15 +9,11 @@ $(document).ready(function(){
   navigator.mediaDevices.getUserMedia(constraints).
   then((stream) => {video.srcObject = stream});
   $('#image').hide();
-  $('#vid').click(function(){
+  $('#scale-element').click(function(){
     var canvas = document.querySelector('canvas');
     canvas.getContext('2d').drawImage(video,0,0);
     var features = [{
       "type": "TYPE_UNSPECIFIED",
-      "maxResults": 1
-  },
-    {
-      "type": "LANDMARK_DETECTION",
       "maxResults": 1
   },
     {
@@ -37,27 +33,17 @@ $(document).ready(function(){
       "maxResults": 5
   },
     {
-      "type": "SAFE_SEARCH_DETECTION",
-      "maxResults": 1
-  },
-    {
-      "type": "IMAGE_PROPERTIES",
-      "maxResults": 1
-  },
-    {
-      "type": "CROP_HINTS",
-      "maxResults": 1
-  },
-    {
-      "type": "WEB_DETECTION",
+      "type": "DOCUMENT_TEXT_DETECTION",
       "maxResults": 1
   }
  ]
-    var requestObject = {}
+    var requestObject = {};
     requestObject.requests = [{}];
-    requestObject.requests[0].image={}
+    requestObject.requests[0].image={};
     requestObject.requests[0].image.content = canvas.toDataURL().substring(22);
     requestObject.requests[0].features =  features;
+    requestObject.requests.imageContext = {};
+    requestObject.requests.imageContext.languageHints = ["en-t-i0-handwrit"];
     var jsonObject = JSON.stringify(requestObject);
     console.log(jsonObject);
     $.ajax({
@@ -70,5 +56,55 @@ $(document).ready(function(){
       }
     });
   });
+  var scale = 1,
+  gestureArea = document.getElementById('gesture-area'),
+  scaleElement = document.getElementById('scale-element'),
+  resetTimeout;
+
+interact(gestureArea)
+.gesturable({
+  onstart: function (event) {
+    clearTimeout(resetTimeout);
+    scaleElement.classList.remove('reset');
+  },
+  onmove: function (event) {
+    scale = scale * (1 + event.ds);
+
+    scaleElement.style.webkitTransform =
+    scaleElement.style.transform =
+      'scale(' + scale + ')';
+
+    dragMoveListener(event);
+  },
+  onend: function (event) {
+    
+  }
+})
+.draggable({ onmove: dragMoveListener });
+
+function reset () {
+scale = 1;
+scaleElement.style.webkitTransform =
+scaleElement.style.transform =
+  'scale(1)';
+}
+function dragMoveListener (event) {
+  var target = event.target,
+      // keep the dragged position in the data-x/data-y attributes
+      x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx,
+      y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
+  // translate the element
+
+    target.style.transform =
+      'translate(' + x + 'px, ' + y + 'px)';
+
+  // update the posiion attributes
+  target.setAttribute('data-x', x);
+  target.setAttribute('data-y', y);
+}
+
+// this is used later in the resizing and gesture demos
+window.dragMoveListener = dragMoveListener;
+
 
 });
